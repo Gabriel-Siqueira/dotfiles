c8e76c9f4c7f8ccaa768beda292598065d695187
Gabriel-Siqueira <gabriel._gabriel@hotmail.com>
2016-04-28 17:09:17 -0300

    add install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+# create links to use files in .dotfiles folder
+ln -s ~/.dotfiles/.bash_profile     ~/.bash_profile
+ln -s ~/.dotfiles/.bashrc           ~/.bashrc
+ln -s ~/.dotfiles/.cvimrc           ~/.cvimrc
+ln -s ~/.dotfiles/.emacs            ~/.emacs
+ln -s ~/.dotfiles/.inputrc          ~/.inputrc
+ln -s ~/.dotfiles/.vimrc            ~/.vimrc
+ln -s ~/.dotfiles/.vimperatorrc     ~/.vimperatorrc
+ln -s ~/.dotfiles/.awsesome/rc.lua  ~/.config/awesome/rc.lua
