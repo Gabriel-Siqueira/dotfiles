523f6264db4dd17174664b6bfc58d953d8429ae1
ra155446 <ra155446@bolivia.redes.lab.ic.unicamp.br>
2016-05-12 11:26:12 -0300

    improviment on emacs init file

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,15 +1,37 @@
-(org-babel-load-file "~/.emacs.d/emacs.org")
+;;; init.el --- Where all the magic begins
+;;
+;; This file loads Org-mode and then loads the rest of our Emacs initialization from Emacs lisp
+;; embedded in literate Org-mode files.
+
+;; Load up Org Mode and (now included) Org Babel for elisp embedded in Org Mode files
+(setq dotfiles-dir (file-name-directory (or (buffer-file-name) load-file-name)))
+
+(let* ((org-dir (expand-file-name
+                 "lisp" (expand-file-name
+                         "org" (expand-file-name
+                                "src" dotfiles-dir))))
+       (org-contrib-dir (expand-file-name
+                         "lisp" (expand-file-name
+                                 "contrib" (expand-file-name
+                                            ".." org-dir))))
+       (load-path (append (list org-dir org-contrib-dir)
+                          (or load-path nil))))
+  ;; load up Org-mode and Org-babel
+  (require 'org-install)
+  (require 'ob-tangle))
+
+;; load up all literate org-mode files in this directory
+(mapc #'org-babel-load-file (directory-files dotfiles-dir t "\\.org$"))
+
+;;; init.el ends here
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-	 [default default default italic underline success warning error])
+ '(ansi-color-faces-vector [default default default italic underline success warning error])
  '(blink-cursor-mode nil)
- '(custom-safe-themes
-	 (quote
-		("1fcc7bd661ad59252a56db9105868d254b15c69ed5ebbdeff39f2793036d4d07" "e033590b106c4cc03a7a87398f0a925f60c29d29686ac9a3f3e925c13656654b" "1137f7051c2e0c81dc6574f01413f7bce148299bd3ccf8ed87051d2b0fdc10b3" "464c5501b011c289408405c4526e0898c8b7dfa53bb81e7319f4ebf1c80b129d" "61d2f35ff5835a13802799ace96fea52e5c3eb947087950e688af51a0c8f113d" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
+ '(custom-safe-themes (quote ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
  '(display-time-mode t)
  '(inhibit-startup-screen t)
  '(menu-bar-mode nil)
