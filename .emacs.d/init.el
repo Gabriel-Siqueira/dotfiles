9b69921488cb2553610624c11d238f805b946fca
Gabriel-Siqueira <gabriel._gabriel@hotmail.com>
2016-05-20 14:08:38 -0300

    emacs spaceline and other additions

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -22,25 +22,3 @@
 
 ;; load up all literate org-mode files in this directory
 (org-babel-load-file "~/.emacs.d/emacs.org")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-	 [default default default italic underline success warning error])
- '(blink-cursor-mode nil)
- '(custom-safe-themes
-	 (quote
-		("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "1fcc7bd661ad59252a56db9105868d254b15c69ed5ebbdeff39f2793036d4d07" default)))
- '(display-time-mode t)
- '(inhibit-startup-screen t)
- '(menu-bar-mode nil)
- '(tool-bar-mode nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Terminus" :foundry "xos4" :slant normal :weight bold :height 105 :width normal))))
- '(buffer-menu-buffer ((t (:weight bold)))))
