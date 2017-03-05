d0782f5d560748e576bf6bd6b21ebc9d54c849db
Gabriel-Siqueira <gabriel._gabriel@hotmail.com>
2017-03-05 18:22:31 -0300

    install.sh now remove old files/links

--- install.sh
+++ install.sh
@@ -1,36 +1,34 @@
 # create links to use files in .dotfiles folder
-ln -s ~/MEGA/dotfiles/bash_profile       ~/.bash_profile
-ln -s ~/MEGA/dotfiles/bash_profile       ~/.profile
-ln -s ~/MEGA/dotfiles/shell/bashrc       ~/.bashrc
+# rm -i ~/.bash_profile && ln -s ~/MEGA/dotfiles/bash_profile       ~/.bash_profile
+# rm -i ~/.profile      && ln -s ~/MEGA/dotfiles/bash_profile       ~/.profile
+# rm -i ~/.bashrc       && ln -s ~/MEGA/dotfiles/shell/bashrc       ~/.bashrc
 
-ln -s ~/MEGA/dotfiles/shell/zsh/zshrc           ~/.zshrc
-ln -s ~/MEGA/dotfiles/shell/zsh/my.zsh-theme    ~/.oh-my-zsh/themes/my.zsh-theme
-ln -s ~/MEGA/dotfiles/shell/zsh/my_config.zsh   ~/.oh-my-zsh/custom/my_config.zsh
+# rm -i ~/.zshrc                          && ln -s ~/MEGA/dotfiles/shell/zsh/zshrc           ~/.zshrc
+# rm -i ~/.oh-my-zsh/themes/my.zsh-theme  && ln -s ~/MEGA/dotfiles/shell/zsh/my.zsh-theme    ~/.oh-my-zsh/themes/my.zsh-theme
+# rm -i ~/.oh-my-zsh/custom/my_config.zsh && ln -s ~/MEGA/dotfiles/shell/zsh/my_config.zsh   ~/.oh-my-zsh/custom/my_config.zsh
 
-ln -s ~/MEGA/dotfiles/shell/config.fish         ~/.config/fish/config.fish
+# rm -i ~/.config/fish/config.fish        && ln -s ~/MEGA/dotfiles/shell/config.fish         ~/.config/fish/config.fish
 
-ln -s ~/MEGA/dotfiles/editors/emacs.d/emacs.org  ~/.emacs.d/emacs.org
-ln -s ~/MEGA/dotfiles/editors/emacs.d/init.el    ~/.emacs.d/init.el
-ln -s ~/MEGA/dotfiles/editors/emacs.d/mysnippets ~/.emacs.d/mysnippets
+# rm -i ~/.emacs.d/emacs.org  && ln -s ~/MEGA/dotfiles/editors/emacs.d/emacs.org  ~/.emacs.d/emacs.org
+# rm -i ~/.emacs.d/init.el    && ln -s ~/MEGA/dotfiles/editors/emacs.d/init.el    ~/.emacs.d/init.el
+# rm -i ~/.emacs.d/mysnippets && ln -s ~/MEGA/dotfiles/editors/emacs.d/mysnippets ~/.emacs.d/mysnippets
 
-ln -s ~/MEGA/dotfiles/editors/inputrc              ~/.inputrc
+# rm -i ~/.inputrc && ln -s ~/MEGA/dotfiles/editors/inputrc              ~/.inputrc
 
-ln -s ~/MEGA/dotfiles/editors/vimrc                ~/.vimrc
-ln -s ~/MEGA/dotfiles/editors/init.vim             ~/.config/nvim/init.vim
-ln -s ~/MEGA/dotfiles/editors/vim/mysnippets       ~/.vim/mysnippets
-ln -s ~/MEGA/dotfiles/browser_vim/vimperatorrc    ~/.vimperatorrc
-# ln -s ~/MEGA/dotfiles/browser_vim/dark.vimp       ~/.vimperator/colors/dark.vimp
-ln -s ~/MEGA/dotfiles/browser_vim/cvimrc          ~/.cvimrc
+# rm -i ~/.vimrc                       && ln -s ~/MEGA/dotfiles/editors/vimrc                ~/.vimrc
+# rm -i ~/.config/nvim/init.vim        && ln -s ~/MEGA/dotfiles/editors/init.vim             ~/.config/nvim/init.vim
+# rm -i ~/.vim/mysnippets              && ln -s ~/MEGA/dotfiles/editors/vim/mysnippets       ~/.vim/mysnippets
+# rm -i ~/.vimperatorrc                && ln -s ~/MEGA/dotfiles/browser_vim/vimperatorrc     ~/.vimperatorrc
+# rm -i ~/.vimperator/colors/dark.vimp && ln -s ~/MEGA/dotfiles/browser_vim/dark.vimp        ~/.vimperator/colors/dark.vimp
+# rm -i ~/.cvimrc                      && ln -s ~/MEGA/dotfiles/browser_vim/cvimrc          ~/.cvimrc
 
-# ln -s ~/MEGA/dotfiles/wm/xmonad/xmonad.hs  ~/.xmonad/xmonad.hs
-# ln -s ~/MEGA/dotfiles/wm/xmonad/volume.sh  ~/.xmonad/volume.sh
-ln -s ~/MEGA/dotfiles/wm/xmonad/xmobarrc   ~/.xmobarrc
+# rm -i ~/.xmonad/xmonad.hs && ln -s ~/MEGA/dotfiles/wm/xmonad/xmonad.hs  ~/.xmonad/xmonad.hs
+# rm -i ~/.xmobarrc         && ln -s ~/MEGA/dotfiles/wm/xmonad/xmobarrc   ~/.xmobarrc
 
-ln -s ~/MEGA/dotfiles/wm/i3/conkyrc         ~/.conkyrc
-# ln -s ~/MEGA/dotfiles/wm/i3/i3blocks       ~/.config/i3blocks/config
-# ln -s ~/MEGA/dotfiles/wm/i3/i3status.conf  ~/.i3status.conf
+# rm -i ~/.conkyrc       && ln -s ~/MEGA/dotfiles/wm/i3/conkyrc         ~/.conkyrc
+# rm -i ~/.i3status.conf && ln -s ~/MEGA/dotfiles/wm/i3/i3status.conf   ~/.i3status.conf
 
-ln -s ~/MEGA/dotfiles/stalonetrayrc        ~/.stalonetrayrc
+# rm -i ~/.stalonetrayrc && ln -s ~/MEGA/dotfiles/stalonetrayrc    ~/.stalonetrayrc
 
-ln -s ~/MEGA/dotfiles/Xresources          ~/.Xresources
-ln -s ~/MEGA/dotfiles/shell/tmux.conf     ~/.tmux.conf
+# rm -i ~/.Xresources && ln -s ~/MEGA/dotfiles/Xresources          ~/.Xresources
+# rm -i ~/.tmux.conf  && ln -s ~/MEGA/dotfiles/shell/tmux.conf     ~/.tmux.conf
