e90a54539b185868aa0f5beb33d8eb2c55048174
Gabriel Siqueira <gabriel.gabrielhs@gmail.com>
2020-05-31 20:52:23 -0300

    Small Fix on set_task and Chomium on Vim

--- bin/set_task.sh
+++ bin/set_task.sh
@@ -9,7 +9,7 @@ if [ $# -ge 1 ]
 then
   id=$1 
 else
-  col='substr($0,prev["ID"],len["ID"]),substr($0,prev["Description"],len["Description"])'
+  col='substr($0,prev["ID"],len["ID"]),substr($0,prev["Description"],500)'
   tasks=$(task daily | awk "BEGIN{OFS=\"\\t\"} NR==2 {for(i=1;i<=NF;i++){name[i]=\$i}} NR==3{acc=0;for(i=1;i<=NF;i++){prev[name[i]]=acc;len[name[i]]=length(\$i);acc+=1+length(\$i)}} NR>3{print $col}" - | head -n -2)
   tasks+=$'\n'
   tasks+=$(cat $MY_OTHER_ACT | sed -E "s/^/-\t/")
