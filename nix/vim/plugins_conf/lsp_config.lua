40e02f7e2038fdbd7ade1c95fafda8f3deb60bfa
Gabriel-Siqueira <gabriel.gabrielhs@gmail.com>
2025-04-13 19:39:32 -0300

    Add key to languagetool

--- nix/vim/plugins_conf/lsp_config.lua
+++ nix/vim/plugins_conf/lsp_config.lua
@@ -1,3 +1,17 @@
+-- Read API key from file
+local function read_api_key_from_file(file_path)
+  local file = io.open(file_path, "r")
+  if not file then
+    return nil
+  end
+  local api_key = file:read("*l") -- read first line
+  file:close()
+  return api_key and api_key:gsub("^%s*(.-)%s*$", "%1") -- trim whitespace
+end
+
+-- Get API key
+local api_key = read_api_key_from_file(vim.fn.expand("$DROPBOX/Backup/pc/languageToolKey.txt"))
+
 vim.g.lsplist = {
     -- 'agda_ls',
     'ccls',
@@ -11,7 +25,18 @@ vim.g.lsplist = {
 -- require 'lspconfig'.agda_ls.setup {}
 require 'lspconfig'.ccls.setup { compilationDatabaseDirectory = "build", index = { threads = 0, }, }
 require 'lspconfig'.hls.setup { formattingProvider = "ourmolu", plugin = { ormolu = { config = { external = true, }, }, }, }
-require 'lspconfig'.ltex.setup { settings = { ltex = { language = "auto", }, } }
+require 'lspconfig'.ltex.setup {
+    settings = {
+        ltex = {
+            language = "auto",
+            languageToolHttpServerUri='https://api.languagetoolplus.com',
+            languageToolOrg = {
+                username='gabriel.gabrielhs@gmail.com',
+                apiKey = api_key,
+            }
+        },
+    }
+}
 require 'lspconfig'.lua_ls.setup {}
 require 'lspconfig'.nil_ls.setup {}
 require 'lspconfig'.pyright.setup {}
